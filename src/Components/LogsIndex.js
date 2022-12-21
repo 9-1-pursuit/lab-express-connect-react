e59f735be4993b09ac9a5238421a8b918d41cb7f
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-21 12:36:04 -0500

    success fetch data using axios from local server in Provider component

--- src/Components/LogsIndex.js
+++ src/Components/LogsIndex.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { ContextData } from "./Provider";
 import axios from "axios";
 import "./LogsIndex.css"
 
 function LogsIndex(props) {
+    const {logs, setLogs} = useContext(ContextData)
+
+
+    useEffect(() => {
+
+    }, [])
     return (
         <div className="index">
             index file
