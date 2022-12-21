6d77f66abed78223cf70b7cf41c1f4d18c422748
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-21 12:45:07 -0500

    able to map out data on logsIndex

--- src/Components/LogsIndex.js
+++ src/Components/LogsIndex.js
@@ -1,19 +1,24 @@
 import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import uuid from "react-uuid";
 import { ContextData } from "./Provider";
-import axios from "axios";
 import "./LogsIndex.css"
 
 function LogsIndex(props) {
     const {logs, setLogs} = useContext(ContextData)
 
-
-    useEffect(() => {
-
-    }, [])
     return (
         <div className="index">
-            index file
+            <section className="logs">
+                {
+                    logs.map(({captainName, title }) => 
+                        <div key = {uuid()}>
+                            <p>{captainName}</p>
+                            <p>{title}</p>
+                        </div>
+                    )
+                }
+            </section>
         </div>
     );
 }
