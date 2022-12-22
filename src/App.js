0a8d35836102816c34c212691cc2474eb48ab1cb
bowersjuan <bowersjuan@gmail.com>
2022-12-22 10:45:58 -0500

    Update-Routes(1) update routes for App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./common/Navbar";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/logs" element={"All Logs"} />
+        <Route path="/logs/new" element={"Create New Log Form"} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
