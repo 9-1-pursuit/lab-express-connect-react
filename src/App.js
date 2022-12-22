56cfc77ad413d638d9c745902fe1f8ba2065f681
bowersjuan <bowersjuan@gmail.com>
2022-12-22 11:00:27 -0500

    create-home-component(1) Create,  style  and import Home component into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./common/Navbar";
+import Home from "./pages/Home";
+
+console.log(<Home />);
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Routes>
-        <Route path="/" element={"Home"} />
+        <Route path="/" element={<Home />} />
         <Route path="/logs" element={"All Logs"} />
         <Route path="/logs/new" element={"Create New Log Form"} />
       </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to the Captain's Log</h2>;
+      <p>Copyright © 2022 Bowers and Sons</p>
+    </div>
+  );
+}
