ce2a450f2b2f4eed8f070e789a6be484db16c169
bowersjuan <bowersjuan@gmail.com>
2022-12-23 17:28:39 -0500

    update app.js routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./common/Navbar";
 import Home from "./pages/Home";
-
-console.log(<Home />);
+import Index from "./pages/Index";
+import Show from "./pages/Show";
+import NotFound from "./pages/NotFound.js";
 
 function App() {
   return (
@@ -10,8 +11,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/logs" element={"All Logs"} />
+        <Route path="/logs" element={<Index />} />
+        <Route path="/logs/:index" element={<Show />} />
         <Route path="/logs/new" element={"Create New Log Form"} />
+        <Route path="/not-found" element={<NotFound />} />
       </Routes>
     </div>
   );
