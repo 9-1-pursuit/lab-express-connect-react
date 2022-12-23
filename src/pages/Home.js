c7f38ac762e6bce73714da14ff42744fd57e3e06
bowersjuan <bowersjuan@gmail.com>
2022-12-23 16:20:27 -0500

    update-home.js(1) Update Home component and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h2>Welcome to the Captain's Log</h2>;
+      <h2>Welcome to the Captain's Log</h2>
       <p>Copyright © 2022 Bowers and Sons</p>
     </div>
   );
