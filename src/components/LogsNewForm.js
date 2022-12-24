4548bbb7d6bd79f2734054d10985e17acf732dbe
bowersjuan <bowersjuan@gmail.com>
2022-12-24 03:03:29 -0500

    update-logs-new-form-component Update LogsNewForm component with useNavigate function for form submission

--- src/components/LogsNewForm.js
+++ src/components/LogsNewForm.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./LogsNewForm.css";
 
