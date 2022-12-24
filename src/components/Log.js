5e91f62d5b743887222a1e019a64607996a2462c
bowersjuan <bowersjuan@gmail.com>
2022-12-24 02:41:33 -0500

    create-footer-for-log-component Create footer html for Log component with Back, Edit and Delete buttons without functionality

--- src/components/Log.js
+++ src/components/Log.js
@@ -3,26 +3,41 @@ import "./Log.css";
 export default function Logs({ log, index }) {
   return (
     <div className="log-container">
-      <h4 style={{ fontFamily: "monospace" }}>
-        Log #{Number(index) + Number(Math.random().toFixed(3)) + "." + index}
-      </h4>
-      <p
-        className="
+      <body>
+        <h4 style={{ fontFamily: "monospace" }}>
+          Log #{Number(index) + Number(Math.random().toFixed(3)) + "." + index}
+        </h4>
+        <p
+          className="
         black-border">
-        Title: <strong>{log.title}</strong>{" "}
-      </p>
-      <p className="black-border">
-        Captain: <em>{log.captainName}</em>
-      </p>
-      <p className="black-border">
-        Mistakes: {log.mistakesWereMadeToday ? "🔴 Yes" : "🟢 No"}
-      </p>
-      <p className="black-border">
-        Days since last Crisis: {log.daysSinceLastCrisis}
-      </p>
-      <p className="black-border" id="log-post">
-        Post: {log.post}
-      </p>
+          Title: <strong>{log.title}</strong>{" "}
+        </p>
+        <p className="black-border">
+          Captain: <em>{log.captainName}</em>
+        </p>
+        <p className="black-border">
+          Mistakes: {log.mistakesWereMadeToday ? "🔴 Yes" : "🟢 No"}
+        </p>
+        <p className="black-border">
+          Days since last Crisis: {log.daysSinceLastCrisis}
+        </p>
+        <p className="black-border" id="log-post">
+          Post: {log.post}
+        </p>
+      </body>
+      <br></br>
+      <hr></hr>
+      <footer id="log-buttons-footer">
+        <label htmlFor="backButton">
+          <button id="backButton">Back</button>
+        </label>
+        <label htmlFor="editButton">
+          <button id="editButton">Edit</button>
+        </label>
+        <label htmlFor="deleteButton">
+          <button id="deleteButton">Delete</button>
+        </label>
+      </footer>
     </div>
   );
 }
