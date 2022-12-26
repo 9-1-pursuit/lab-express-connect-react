6f4fbc37e0186ba11a110f3223ce1cecc6163c5e
MarsiaRochelin <marsrochelin@pursuit.org>
2022-12-26 17:05:41 -0500

    tweak

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import New from "./components/New";
-import Delete from "./components/Delete";
 import Index from "./components/Index";
+import Show from "./components/Show";
+import New from "./components/New";
 import Edit from "./components/Edit";
+import Delete from "./components/Delete";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/logs" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/logs" element={<Index />} />
           <Route path="/logs/new" element={<New />} />
           <Route path="/logs/delete" element={<Delete />} />
-          <Route path="/logs/:index" element={<Index />} />
+          <Route path="/logs/:index" element={<Show />} />
           <Route path="/logs/:index/edit" element={<Edit />} />
         </Routes>
       </Router>
