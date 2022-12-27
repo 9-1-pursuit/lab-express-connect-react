59a9430dc32b91eb0a3568c120c993ae5ddb30ce
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2022-12-27 12:12:24 -0500

    finished edit form

--- src/Components/LogsDetails.js
+++ src/Components/LogsDetails.js
@@ -1,3 +1,77 @@
-export default function logsDetail() {
-  return <div>logs detail</div>;
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+const API = process.env.REACT_APP_API_URL;
+
+function LogsDetails() {
+  const [log, setLog] = useState([]);
+  let { index } = useParams();
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/logs/${index}`)
+      .then((res) => setLog(res.data))
+      .catch((err) => console.log(err));
+  }, [index, navigate]);
+
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/logs/${index}`)
+      .then(() => navigate(`/logs`))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <h3>
+        {log.captainName ? (
+          <span>
+            {' '}
+            <b>Name</b>: {log.captainName}
+          </span>
+        ) : null}
+      </h3>
+      <p>
+        {log.title ? (
+          <span>
+            {' '}
+            <b>Title</b>: {log.title}
+          </span>
+        ) : null}
+      </p>
+      <p>
+        {log.post ? (
+          <span>
+            {' '}
+            <b>Post</b>: {log.post}
+          </span>
+        ) : null}
+      </p>
+      <p>
+        {log.daysSinceLastCrisis ? (
+          <span>
+            <b>Days Since Last Crisis</b>:{log.daysSinceLastCrisis}
+          </span>
+        ) : null}
+      </p>
+      <div>
+        {' '}
+        <Link to={`/logs`}>
+          <button>Back</button>
+        </Link>
+      </div>
+      <div>
+        {' '}
+        <Link to={`/logs/${index}/edit`}>
+          <button>Edit</button>
+        </Link>
+      </div>
+      <div>
+        {' '}
+        <button onClick={handleDelete}>Delete</button>
+      </div>
+    </div>
+  );
 }
+
+export default LogsDetails;
--- src/Components/LogsEditForm.js
+++ src/Components/LogsEditForm.js
@@ -1,3 +1,93 @@
+import { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+const API = process.env.REACT_APP_API_URL;
 export default function LogsEditForm() {
-  return <div>edit</div>;
+  let { index } = useParams();
+  const navigate = useNavigate();
+
+  const [log, setLog] = useState({
+    captainName: '',
+    title: '',
+    post: '',
+    mistakesWereMadeToday: false,
+    daysSinceLastCrisis: 0,
+    // daysSinceLastCrisis: '',
+  });
+  const handleTextChange = (event) => {
+    setLog({ ...log, [event.target.id]: event.target.value });
+  };
+
+  function checkbox() {
+    return setLog({
+      ...log,
+      mistakesWereMadeToday: !log.mistakesWereMadeToday,
+    });
+  }
+  useEffect(() => {
+    axios
+      .get(`${API}/logs/${index}`)
+      .then((res) => setLog(res.data))
+      .catch((err) => console.log(err));
+  }, [index]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`${API}/logs/${index}`, log)
+      .then((res) => {
+        setLog(res.data);
+        navigate(`/logs/${index}`);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="captainName"
+          type="text"
+          value={log.captainName}
+          onChange={handleTextChange}
+          placeholder="Name"
+          required
+        />
+        <label htmlFor="title">Title:</label>
+        <input
+          id="title"
+          type="text"
+          value={log.title}
+          onChange={handleTextChange}
+          placeholder="title"
+          required
+        />
+        <label htmlFor="post">Post:</label>
+        <input
+          id="post"
+          type="text"
+          value={log.post}
+          onChange={handleTextChange}
+          placeholder="post"
+          required
+        />
+        <label htmlFor="daysSinceLastCrisis">Days Since Last Crisis:</label>
+        <input
+          id="daysSinceLastCrisis"
+          type="number"
+          value={log.daysSinceLastCrisis}
+          onChange={handleTextChange}
+          placeholder="daysSinceLastCrisis"
+          required
+        />
+        <label htmlFor="checkbox">Mistakes</label>
+        <input
+          checked={log.mistakesWereMadeToday}
+          type="checkbox"
+          id="mistakesWereMadeToday"
+          onChange={checkbox}
+        ></input>
+        <input type="submit"></input>
+      </form>
+    </div>
+  );
 }
