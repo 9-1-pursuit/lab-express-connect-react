29448598da72a630f6937f40584c3888ff19850a
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-29 17:42:16 -0500

    added all inputs, and post request on submit for form, conditional to convert daysSinceCrisis to a number in form

--- src/Components/LogsNewForm.js
+++ src/Components/LogsNewForm.js
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ContextData } from "./Provider";
+import { useNavigate } from "react-router-dom";
 import "./LogsNewForm.css"
+
 
 function LogsNewForm() {
 /* 
@@ -9,7 +12,8 @@ post (text)
 mistakesWereMadeToday (checkbox)
 daysSinceLastCrisis (number)
 submit (submit) */
-
+const {API, axios} = useContext(ContextData)
+const navigate = useNavigate()
 // declare state for checkbox
 const [checkbox, setCheckbox] = useState(false)
 // declare state for new form input
@@ -17,12 +21,30 @@ const [newForm, setNewForm] = useState({
     captainName: "",
     title: "",
     post: "",
-    mistakesWereMadeToday: checkbox,
-    daysSinceLastCrisis: null,
+    mistakesWereMadeToday: undefined,
+    daysSinceLastCrisis: undefined,
 })
 // function for handling checkbox change
 function handleCheckbox() {
     setCheckbox(!checkbox)
+    newForm.mistakesWereMadeToday = !checkbox
+}
+// function for handling text input change
+function handleTextChange(e) {
+    let value = e.target.value
+    const id = e.target.id
+    if(id === "daysSinceLastCrisis"){
+        value = +e.target.value
+    }
+    setNewForm({...newForm, [id]: value})
+}
+//  function for handling form submit
+function handleSubmit(e) {
+    e.preventDefault()
+    // use post request, 1st arg: url, 2nd arg: data to send
+    axios.post(`${API}`, newForm)
+    .then(() => navigate("/logs"))
+    .catch(err => console.log(err))
 }
 
 
@@ -31,12 +53,70 @@ function handleCheckbox() {
 
     return (
         <div className='new'>
-            new form
-            <input 
-            type= "checkbox"
-            checked = {checkbox}
-            onChange = {() => handleCheckbox()}
-            />
+           <form 
+           onSubmit={(event) => handleSubmit(event)}>
+            {/* Name */}
+            <label htmlFor="captainName">Captain Name: 
+                <input 
+                id = "captainName"
+                type = "text"
+                value = {newForm.captainName}
+                onChange = {(event) => handleTextChange(event)}
+                />
+            </label>
+            <br></br>
+
+            {/* title */}
+            <label htmlFor="title">Title: 
+                <input 
+                id = "title"
+                type = "text"
+                value = {newForm.title}
+                onChange = {(event) => handleTextChange(event)}
+                />
+            </label>
+            <br></br>
+
+            {/* post */}
+            <label htmlFor="post">Post: 
+                <input 
+                id = "post"
+                type = "text"
+                value = {newForm.post}
+                onChange = {(event) => handleTextChange(event)}
+                />
+            </label>
+            <br></br>
+            
+            {/* Mistakes */}
+            <label
+            htmlFor="mistakes">Mistakes Made Today?
+                <input
+                id = "mistakes" 
+                type= "checkbox"
+                checked = {checkbox}
+                onChange = {() => handleCheckbox()}
+                />
+            </label>
+            <br></br>
+
+            {/* days Since crisis */}
+            <label htmlFor="daysSinceLastCrisis">Days Since Last Crisis: 
+                <input 
+                id = "daysSinceLastCrisis"
+                type = "number"
+                value = {newForm.daysSinceLastCrisis}
+                onChange = {(event) => handleTextChange(event)}
+                />
+            </label>
+            <br></br>
+
+            {/* submit button */}
+            <input
+            type = "submit" />
+
+        </form> 
+            
         </div>
     );
 }
