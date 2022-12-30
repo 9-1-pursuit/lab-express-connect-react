9f17d0b5c3d49ad709668561fa9533acbaa467b2
sreeves07 <shamarreeves@pursuit.org>
2022-12-30 15:27:22 -0500

    routes added and components created

---
+++ Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar(props) {
+    return (
+        <nav>
+            <h1>
+                <Link to='/logs'>Captain's Log</Link>
+            </h1>
+            <button>
+                <Link to='/logs/new'>Create New Log</Link>
+            </button>    
+        </nav>
+    );
+}
+
+export default NavBar;
--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "../Components/NavBar";
+import Home from "../Components/NavBar";
+import Logs from "../Components/NavBar";
+import LogDetails from "../Components/NavBar";
+import LogEdit from "../Components/NavBar";
+import LogNew from "../Components/NavBar";
+
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <div className="Routes">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Logs />} />
+            <Route path="/logs/:index" element={<LogDetails />} />
+            <Route path="/logs/:index/edit" element={<LogEdit />} />
+            <Route path="/logs/new" element={<LogNew />} />
+            <Route path="*" />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
