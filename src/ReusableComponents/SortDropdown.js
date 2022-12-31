c50023b0beb1a672a7f26ff7455724e4e2e3b668
DestinyJoyner <destinyjoyner@pursuit.org>
2022-12-31 00:13:52 -0500

    dropdown filters sort functioning, need to move state, logs show useeefect overrides sorted order need tofix

--- src/ReusableComponents/SortDropdown.js
+++ src/ReusableComponents/SortDropdown.js
@@ -1,26 +1,37 @@
 import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { ContextData } from "../Components/Provider";
-
 
 function SortDropdown({setLogs}) {
     // declare state for select value
-    const [select, setSelect] = useState("")
+    const [select, setSelect] = useState("default")
     const {API, axios} = useContext(ContextData)
+    const navigate = useNavigate()
 
     // function for handling dropdown selection
     function handleDropdown(e) {
         const value = e.target.value
         setSelect(value)
-        if(value === "asc" || value === "desc"){
-            axios.get(`${API}?order=${value}`)
-            .then(respJson => setLogs(respJson.data))
-        }
+        let type; 
+        if(value === "asc" || value === "desc") type = "order"
+        if(value === "true" || value === "false") type = "mistakes"
+        if(value === "gt10" || value === "gte20" || value === "lte5") type = "lastCrisis"
+
+        let query = !type ? `` : `?${type}=${value}`
+
+        axios.get(`${API}${query}`)
+        .then(respJson => setLogs(respJson.data))
+        .catch(err => navigate("/*") )
     }
+
     return (
+        <>
+        <label htmlFor="dropdown">Sort Logs By: </label>
         <select
         value = {select}
-        onChange = {(event) => handleDropdown(event)}>
-            <option value = "">Sort Logs By</option>
+        onChange = {(event) => handleDropdown(event)}
+        id = "dropdown">
+            <option value = "default">Default</option>
             <option value= "asc">Captain Name Ascending</option>
             <option value = "desc">Captain Name Descending</option>
             <option value = "true">Mistakes Made</option>
@@ -29,6 +40,7 @@ function SortDropdown({setLogs}) {
             <option value = "gte20">At Least 20 Days Since Last Crisis</option>
             <option value = "lte5">5 Or Less Days Since Last Crisis</option>
         </select>
+        </>
     );
 }
 
