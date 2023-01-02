fb6a54ef4affcd095cd084f8b671608f0bfa310b
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-01 20:57:38 -0500

    fin al test app functioning with sort refactoring

--- src/ReusableComponents/helperFunctions.js
+++ src/ReusableComponents/helperFunctions.js
@@ -21,9 +21,7 @@ function handleTextChange(e, stateVar, setFunction) {
         let display;
    
         if(input === "asc"){
-            display = arr.sort((a,b) => 
-                a.captainName.toUpperCase() < b.captainName.toUpperCase() ? -1 : 1 || 0
-            )
+            display = arr.sort((a,b) => a.captainName.toUpperCase() < b.captainName.toUpperCase() ? -1 : 1 || 0)
         }
         if(input === "desc"){
             display = arr.sort((a,b) => a.captainName.toUpperCase() > b.captainName.toUpperCase() ? -1 : 1 || 0)
