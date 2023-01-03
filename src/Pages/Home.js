2fe1b64303baa5e00af4bab44f942c988c7d7d4b
Gillesedgy <edgygilles@pursuit.org>
2023-01-02 19:11:06 -0500

    fixed home page image... might use the other I commentted out

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import "./Home.css"
-
+import { Link } from "react-router-dom"
 function Home() {
   return (
     <div className='Home'><h1>Welcome to Captain's Log</h1>
-    <div className="image">
-      <img src="https://external-preview.redd.it/EpyZ7xaDH520p9zy4xpDrg3u_LYh_EEnkMuytSthEvw.gif?s=c4f8fad1f5cea7af4aa9cf63648adc92227eaf15" alt="" srcset="" />
+    <div className="image">   
+    <Link to={'/logs'}>{/* <img src="https://i.makeagif.com/media/2-11-2016/EXlhA0.gif" alt="gif" srcset="" /> */}
+      {/* <img src="https://cdn.quotesgram.com/img/16/53/1664023620-Kirk-Inspirational-Posters-james-t-kirk-7685921-750-600.jpg" alt="" srcset="" /> */}
+      <img src="https://external-preview.redd.it/oGRvDd0K9oZWO-rcBQRaDNLilZu3G_t9o5jQ4CE4ZyI.jpg?auto=webp&s=73710ce0b5d28bd9825d03f3c4186b84dac1ed38" alt="" srcset="" /></Link>
     </div>
-    </div>
+       </div>
   )
 }
 
