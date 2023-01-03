1a73b8dd16f2f90e397c46d0006993f37ea5f60e
TrianePeart <TrianePeart@gmail.com>
2023-01-03 11:50:07 -0500

    Error fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Nav from './Components/Nav'
 import Home from './Components/Home'
 import Log from './Components/Log'
-
 import Edit from './Components/Forms'
 import Details from './Components/LogDetails'
 
